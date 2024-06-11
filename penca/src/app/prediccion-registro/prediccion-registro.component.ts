c01a385772a0e73bc6fb6e48c561d8d56c637f1f
brunoalbin23 <albinbruno23@gmail.com>
2024-06-11 02:04:27 -0300

    varias cositas

--- penca/src/app/prediccion-registro/prediccion-registro.component.ts
+++ penca/src/app/prediccion-registro/prediccion-registro.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-prediccion-registro',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './prediccion-registro.component.html',
   styleUrl: './prediccion-registro.component.css'
 })
 export class PrediccionRegistroComponent {
+  bandera: boolean = false;
 
   constructor(private router: Router) { }
 
   navigateToSala() {
     this.router.navigate(['/sala-general']);
+  }
+
+  setBanderaLogin() {
+    this.bandera = !this.bandera;
+  }
+
+  closeForm() {
+    this.bandera = !this.bandera;
   }
 }
